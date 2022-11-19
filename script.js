27c53dd3517c526656ae4183ccd2f40a2271d348
Brandon Ngo <brandonngo56@gmail.com>
2022-11-18 23:48:05 -0500

    fix getNumbers() function

--- script.js
+++ script.js
@@ -1,35 +1,67 @@
-var displayValue = "";
+var displayValue = "12+3";
 
 function populate(character){
     displayValue += character;
     document.getElementById('display').innerHTML = displayValue;
 }
 
-function operate(){
-    var operator, num1, num2, operatorIndex, solution;
+function getOperators(){
+    var operators = [];
     for (var i=0; i<displayValue.length; i++){
-        if (displayValue.charAt(i) == '+'){
-            operatorIndex = i;
-        } else if (displayValue.charAt(i) == '-'){
-            operatorIndex = i;
-        } else if (displayValue.charAt(i) == '*'){
-            operatorIndex = i;
-        } else if (displayValue.charAt(i) == '/'){
-            operatorIndex = i;
+        if (displayValue.charAt(i)=='+' || displayValue.charAt(i)=='-' || displayValue.charAt(i)=='*' || displayValue.charAt(i)=='/'){
+            operators.push(displayValue.charAt(i));
         }
     }
-    operator = displayValue.charAt(operatorIndex);
-    num1 = parseInt(displayValue.substring(0, operatorIndex));
-    num2 = parseInt(displayValue.substring(operatorIndex+1));
-    if (operator == '+'){
-        solution = num1 + num2;
-    } else if (operator == '-'){
-        solution = num1 - num2;
-    } else if (operator == '*'){
-        solution = num1 * num2;
-    } else if (operator == '/'){
-        solution = num1 / num2;
-    }
-    displayValue = solution;
-    document.getElementById("display").innerHTML = solution;
+    return operators;
 }
+
+function getNumbers(){
+    var numbers = [];
+    for (var i=0; i<displayValue.length; i++){
+        if (isNaN(displayValue.charAt(i)) == false){
+            var currentNum = "";
+            while (isNaN(displayValue.charAt(i))==false && i<displayValue.length){
+                currentNum += displayValue.charAt(i);
+                i++;
+            }
+            numbers.push(parseInt(currentNum));
+        }
+    }
+    return numbers;
+}
+
+function operate(){
+    numbers = getNumbers();
+    alert(numbers[0]);
+    alert(numbers[1]);
+
+    // var operator, num1, num2, operatorIndex, solution;
+    // for (var i=0; i<displayValue.length; i++){
+    //     if (displayValue.charAt(i) == '+'){
+    //         operatorIndex = i;
+    //     } else if (displayValue.charAt(i) == '-'){
+    //         operatorIndex = i;
+    //     } else if (displayValue.charAt(i) == '*'){
+    //         operatorIndex = i;
+    //     } else if (displayValue.charAt(i) == '/'){
+    //         operatorIndex = i;
+    //     }
+    // }
+    // operator = displayValue.charAt(operatorIndex);
+    // num1 = parseInt(displayValue.substring(0, operatorIndex));
+    // num2 = parseInt(displayValue.substring(operatorIndex+1));
+    // if (operator == '+'){
+    //     solution = num1 + num2;
+    // } else if (operator == '-'){
+    //     solution = num1 - num2;
+    // } else if (operator == '*'){
+    //     solution = num1 * num2;
+    // } else if (operator == '/'){
+    //     solution = num1 / num2;
+    // }
+    // displayValue = solution;
+    // document.getElementById("display").innerHTML = solution;
+}
+
+
+operate();
