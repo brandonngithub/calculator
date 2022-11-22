9b971d2581da59423b70567011d41a9d2aa3190c
Brandon Ngo <brandonngo56@gmail.com>
2022-11-21 20:38:37 -0500

    update files

--- script.js
+++ script.js
@@ -44,3 +44,23 @@ function operate(){
         document.getElementById('display').innerHTML = "ERROR";
     }
 }
+
+//setting onclicks for calculator buttons
+document.getElementById("zero").addEventListener('click', function(e){ populate('0'); });
+document.getElementById("one").addEventListener('click', function(e){ populate('1'); });
+document.getElementById("two").addEventListener('click', function(e){ populate('2'); });
+document.getElementById("three").addEventListener('click', function(e){ populate('3'); });
+document.getElementById("four").addEventListener('click', function(e){ populate('4'); });
+document.getElementById("five").addEventListener('click', function(e){ populate('5'); });
+document.getElementById("six").addEventListener('click', function(e){ populate('6'); });
+document.getElementById("seven").addEventListener('click', function(e){ populate('7'); });
+document.getElementById("eight").addEventListener('click', function(e){ populate('8'); });
+document.getElementById("nine").addEventListener('click', function(e){ populate('9'); });
+document.getElementById("add").addEventListener('click', function(e){ populate('+'); });
+document.getElementById("subtract").addEventListener('click', function(e){ populate('-'); });
+document.getElementById("multiply").addEventListener('click', function(e){ populate('*'); });
+document.getElementById("divide").addEventListener('click', function(e){ populate('/'); });
+document.getElementById("modulo").addEventListener('click', function(e){ populate('%'); });
+document.getElementById("equals").addEventListener('click', function(e){ operate(); });
+document.getElementById("backspace").addEventListener('click', function(e){ backspace(); });
+document.getElementById("clear").addEventListener('click', function(e){ location.reload(); });
